6068d44a3a7519142c58eea83d98047e61c36c49
Tatiana Kashcha <tkashcha@gmail.com>
2024-01-14 21:04:19 +0200

    test2

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,18 +1,18 @@
 // import Wiget from "../ui/admin/UploadWidget";
-// import CreateForm from "../ui/admin/create-form";
+import CreateForm from "../ui/admin/create-form";
 
 export const metadata = {
   title: "Admin",
   description: "My Admin Page",
 };
 
-// const myPreset = process.env.CLOUDINARY_UPLOAD_PRESET;
+const myPreset = process.env.CLOUDINARY_UPLOAD_PRESET;
 
 export default function Page() {
   return (
     <>
       <p>Admin Page</p>
-      {/* <CreateForm myPreset={myPreset} /> */}
+      <CreateForm myPreset={myPreset} />
       {/* <Wiget myPreset={myPreset} />
       <Wiget myPreset={myPreset} />
       <Wiget myPreset={myPreset} /> */}
