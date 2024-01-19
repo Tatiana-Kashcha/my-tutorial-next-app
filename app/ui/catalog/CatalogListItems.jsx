76f7eb66080ba2754ab7420e4c287aed939d7073
Tatiana Kashcha <tkashcha@gmail.com>
2024-01-19 18:39:45 +0200

    Update CatalogListItems.jsx

--- app/ui/catalog/CatalogListItems.jsx
+++ app/ui/catalog/CatalogListItems.jsx
@@ -1,11 +1,13 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { deleteProduct } from "../../lib/actions";
 
 export const CatalogListItems = ({ car }) => {
   //   const url = car.img_url;
   //   console.log(url);
   //   console.log(car);
+  const deleteProductWithId = deleteProduct.bind(null, car.id);
   return (
     <div>
       <div className="mb-2 relative flex items-center justify-center overflow-hidden block w-[274px] h-[254px] border border-gray-200 ">
@@ -18,14 +20,21 @@ export const CatalogListItems = ({ car }) => {
           //   priority
         />
       </div>
-      <p>Model: {car.make}</p>
-      <p>Price $: {car.prise}</p>
+      <p className="mb-2">Model: {car.make}</p>
+      <p className="mb-2">Price $: {car.prise}</p>
 
-      <Link href={`/catalog/${car.id}`}>
-        <button className="inline-block py-2 px-4 rounded-full font-semibold uppercase text-white bg-green-600 hover:bg-green-500">
-          View
-        </button>
-      </Link>
+      <div className="flex">
+        <Link href={`/catalog/${car.id}`}>
+          <button className="inline-block py-2 px-4 rounded-full font-semibold uppercase text-white bg-green-600 hover:bg-green-500">
+            View
+          </button>
+        </Link>
+        <form action={deleteProductWithId}>
+          <button className="ml-2 inline-block py-2 px-4 rounded-full font-semibold uppercase text-white bg-red-600 hover:bg-red-500">
+            Delete
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
