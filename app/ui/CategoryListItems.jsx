3f7ccbe4571871e2c5ee0a48cc577c34497294ad
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-02 23:34:25 +0200

    Update CategoryListItems.jsx

--- app/ui/CategoryListItems.jsx
+++ app/ui/CategoryListItems.jsx
@@ -5,7 +5,7 @@ export const CategoryListItems = ({ data }) => {
   return (
     <>
       {data.img_url ? (
-        <div className="mb-2 flex items-center justify-center overflow-hidden box-border h-36 w-36 rounded-full border border-gray-600">
+        <div className="mb-2 flex items-center justify-center overflow-hidden box-border h-36 w-36 rounded-full">
           <Image
             src={data.img_url}
             alt="Category"
