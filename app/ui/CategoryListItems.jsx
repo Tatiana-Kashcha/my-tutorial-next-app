5ff47cfcd8b6f293cae3a9b08019fa110f497933
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-07 13:38:34 +0200

    Update CategoryListItems.jsx

--- app/ui/CategoryListItems.jsx
+++ app/ui/CategoryListItems.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-export function CategoryListItems({ data }) {
+export const CategoryListItems = ({ data }) => {
   return (
-    <>
+    <div className="mb-14">
       {data.img_url ? (
         <div className="mb-3.5 flex items-center justify-center overflow-hidden box-border w-[184px] h-[184px] rounded-full">
           <Image
@@ -24,35 +24,7 @@ export function CategoryListItems({ data }) {
           />
         </div>
       )}
-    </>
+      <p className="text-center">{data.title}</p>
+    </div>
   );
-}
-
-// export const CategoryListItems = ({ data }) => {
-//   return (
-//     <div className="mb-14">
-//       {data.img_url ? (
-//         <div className="mb-3.5 flex items-center justify-center overflow-hidden box-border w-[184px] h-[184px] rounded-full">
-//           <Image
-//             src={data.img_url}
-//             alt={data.title}
-//             className="block object-cover h-full object-center"
-//             width={184}
-//             height={184}
-//           />
-//         </div>
-//       ) : (
-//         <div className="mb-3.5 flex items-center justify-center overflow-hidden box-border w-[184px] h-[184px] rounded-full">
-//           <Image
-//             src={"/images/noImageIcon.jpg"}
-//             alt={data.title}
-//             className="block object-cover h-full object-center"
-//             width={184}
-//             height={184}
-//           />
-//         </div>
-//       )}
-//       <p className="text-center">{data.title}</p>
-//     </div>
-//   );
-// };
+};
