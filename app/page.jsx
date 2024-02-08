cc50d50ce0a8e8e410dfa47b6d04a0dc896c948d
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-09 01:02:50 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,8 @@
-import { fetchCategory, fetchCatalog } from "./lib/actions";
+import {
+  fetchCategory,
+  fetchCatalog,
+  fetchCategoryAndSubcategory,
+} from "./lib/actions";
 import { CatalogList } from "./ui/catalog/CatalogList";
 import { CategoryList } from "./ui/CategoryList";
 
@@ -9,9 +13,12 @@ export const metadata = {
 
 export default async function HomePage() {
   const dataCategory = await fetchCategory();
+  const dataCategoryAndSubcategory = await fetchCategoryAndSubcategory();
   const dataCatalog = await fetchCatalog();
   const stylizationCategoryList =
     "mb-[108px] flex overflow-x-auto list-none gap-6";
+
+  console.log(dataCategoryAndSubcategory);
 
   return (
     <div className="flex flex-col ">
