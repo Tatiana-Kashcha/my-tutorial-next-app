408685d5454a4b341de89691220e4ac37ed1587d
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-09 21:15:59 +0200

    upd icon

--- app/ui/catalog/HeartIconsFavorite.jsx
+++ app/ui/catalog/HeartIconsFavorite.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import IconNoFavorite from "@/public/icons/IconNoFavorite.svg";
-import IconFavorite from "@/public/icons/IconFavorite.svg";
+import iconNoFavorite from "@/public/icons/IconNoFavorite.svg";
+import iconFavorite from "@/public/icons/IconFavorite.svg";
 
 export function HeartIconsFavorite({ isFavorite }) {
   return (
     <div className="w-[32px] h-[32px] flex justify-center items-center">
       {isFavorite ? (
-        <Image src={IconFavorite} alt="Favorite Icon" width={24} height={24} />
+        <Image src={iconFavorite} alt="Favorite Icon" width={24} height={24} />
       ) : (
         <Image
-          src={IconNoFavorite}
+          src={iconNoFavorite}
           alt="Not Favorite Icon"
           width={24}
           height={24}
