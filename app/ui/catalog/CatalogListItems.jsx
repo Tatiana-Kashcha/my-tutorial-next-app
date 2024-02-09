575517297fe1698cb59ed8fd769e4c8a20ec8589
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-09 16:08:58 +0200

    upd PriceCard

--- app/ui/catalog/CatalogListItems.jsx
+++ app/ui/catalog/CatalogListItems.jsx
@@ -10,16 +10,37 @@ export const CatalogListItems = ({ product }) => {
       <div className="p-2 w-[288px] h-[237px] text-center">
         <p className="mb-2 text-lg">{product.make}</p>
         <AvailabilityProduct product={product} />
-        <div className="mb-1 relative flex justify-center">
+
+        <div className="mb-2 relative">
           <PriceCard product={product} />
           <div className="absolute flex justify-center items-center right-[0px] top-[0px]">
             <div className="box-border w-[32px] h-[32px] border border-blue-800"></div>
           </div>
         </div>
-        <p className="mb-2 text-gray-400">({`Zawiera podatek`})</p>
-
-        <p className="mb-2">{product.quantity}Szt</p>
+        <p className="mb-2">Тест: {product.quantity}Szt</p>
       </div>
     </div>
   );
 };
+
+// export const CatalogListItems = ({ product }) => {
+//   return (
+//     <div className="w-[288px] h-[519px]">
+//       <ImageCard product={product} />
+
+//       <div className="p-2 w-[288px] h-[237px] text-center">
+//         <p className="mb-2 text-lg">{product.make}</p>
+//         <AvailabilityProduct product={product} />
+//         <div className="mb-1 relative flex justify-center">
+//           <PriceCard product={product} />
+//           <div className="absolute flex justify-center items-center right-[0px] top-[0px]">
+//             <div className="box-border w-[32px] h-[32px] border border-blue-800"></div>
+//           </div>
+//         </div>
+//         <p className="mb-2 text-gray-400">({`Zawiera podatek`})</p>
+
+//         <p className="mb-2">{product.quantity}Szt</p>
+//       </div>
+//     </div>
+//   );
+// };
--- app/ui/catalog/PriceCard.jsx
+++ app/ui/catalog/PriceCard.jsx
@@ -5,22 +5,50 @@ export function PriceCard({ product }) {
 
   return (
     <>
-      {product.discount ? (
-        <div className="flex items-center">
-          <p className="mr-1 text-gray-400 text-right line-through">
-            {price.toFixed(2).replace(".", ",")}zł
-          </p>
-          <p className="text-left text-2xl text-blue-800">
-            {discountedPrice.toFixed(2).replace(".", ",")}zł
-          </p>
-        </div>
-      ) : (
-        <div className="flex items-center">
+      <div className="mb-1 flex justify-center items-center">
+        {product.discount ? (
+          <div className="flex items-center">
+            <p className="mr-1 text-gray-400 text-right line-through">
+              {price.toFixed(2).replace(".", ",")}zł
+            </p>
+            <p className="text-left text-2xl text-blue-800">
+              {discountedPrice.toFixed(2).replace(".", ",")}zł
+            </p>
+          </div>
+        ) : (
           <p className="text-2xl text-blue-800">
             {price.toFixed(2).replace(".", ",")}zł
           </p>
-        </div>
-      )}
+        )}
+      </div>
+      <p className="text-gray-400">({`Zawiera podatek`})</p>
     </>
   );
 }
+
+// export function PriceCard({ product }) {
+//   const price = Number(product.price);
+//   const discountAmount = (price * product.discount) / 100;
+//   const discountedPrice = price - discountAmount;
+
+//   return (
+//     <>
+//       {product.discount ? (
+//         <div className="flex items-center">
+//           <p className="mr-1 text-gray-400 text-right line-through">
+//             {price.toFixed(2).replace(".", ",")}zł
+//           </p>
+//           <p className="text-left text-2xl text-blue-800">
+//             {discountedPrice.toFixed(2).replace(".", ",")}zł
+//           </p>
+//         </div>
+//       ) : (
+//         <div className="flex items-center">
+//           <p className="text-2xl text-blue-800">
+//             {price.toFixed(2).replace(".", ",")}zł
+//           </p>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
