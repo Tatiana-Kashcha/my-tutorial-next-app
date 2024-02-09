80f9a3b0dd834eec291703da31ed5b01ca7fe872
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-09 12:26:33 +0200

    Update CatalogListItems.jsx

--- app/ui/catalog/CatalogListItems.jsx
+++ app/ui/catalog/CatalogListItems.jsx
@@ -10,13 +10,25 @@ export const CatalogListItems = ({ product }) => {
       <ImageCard product={product} />
 
       <div className="p-2 w-[288px] h-[237px] text-center">
-        <p className="mb-2">{product.make}</p>
-        <p className="mb-2">Price: {price.toFixed(2).replace(".", ",")}zł</p>
-        <p className="mb-2">
-          DiscountedPrice: {discountedPrice.toFixed(2).replace(".", ",")}zł
-        </p>
+        <p className="mb-2 text-lg">{product.make}</p>
+        {product.discount ? (
+          <div className="mb-1 flex items-center justify-center">
+            <p className="mr-1 text-gray-400 text-right line-through">
+              {price.toFixed(2).replace(".", ",")}zł
+            </p>
+            <p className="text-left text-2xl text-blue-800">
+              {discountedPrice.toFixed(2).replace(".", ",")}zł
+            </p>
+          </div>
+        ) : (
+          <>
+            <p className="mb-2 text-2xl text-blue-800">
+              {price.toFixed(2).replace(".", ",")}zł
+            </p>
+          </>
+        )}
+        <p className="mb-2 text-gray-400">({`Zawiera podatek`})</p>
         <p className="mb-2">{product.quantity}Szt</p>
-        <p className="mb-2">({`Zawiera podatek`})</p>
       </div>
     </div>
   );
