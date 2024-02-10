422fbf5bc59ee6ba6f5b0dd3dffd605d7d04e1eb
Tatiana Kashcha <tkashcha@gmail.com>
2024-02-10 15:03:26 +0200

    Update page.js

--- app/favorite/page.js
+++ app/favorite/page.js
@@ -3,11 +3,13 @@ import { fetchCategoryAndSubcategory } from "../lib/actions";
 import { CategoryOrganize } from "../ui/CategoryOrganize";
 
 export default async function FavoritePage() {
+  // Відповідь з бази по переліку категорій та підкатегорій
   const dataCategoryAndSubcategory = await fetchCategoryAndSubcategory();
   console.log(dataCategoryAndSubcategory);
 
-  // const organizedCategories = organizeCategories(dataCategoryAndSubcategory);
-  // console.log(organizedCategories);
+  // Структура категорій та підкатегорій
+  // const categoryList = organizeCategories(dataCategoryAndSubcategory);
+  // console.log(categoryList);
 
   return (
     <>
